856e26d2082eccf0029f1dc518ccb224a4813e3d
Paarth Gupta <paarth.gupta@hbctxdom.com>
2024-12-04 06:52:33 +0530

    Syncing changes with suggestion

--- src/components/FilterTabs.tsx
+++ src/components/FilterTabs.tsx
@@ -11,69 +11,105 @@ import {
 } from "@mui/material";
 import { Tune } from "@mui/icons-material";
 
-interface FilterAttributes {
-  category: string;
-  options: string[];
+interface TabAttributes {
+  label: string;
+  component: Element;
 }
 
 interface FilterTabsProps {
-  filterList?: FilterAttributes[];
-  handleFilterChange?: (selectedFilter: string) => void;
-  handleTabChange?: (selectedFilter: string, selectedTab: string) => void;
-  computeBadges?: (selectedFilter: string, tabs: string[]) => any;
+  tabList?: TabAttributes[];
+  onTabChange?: (selectedTab: string) => void;
+  // onFilterChange?: (selectedFilter: string) => void;
+  // computeBadges?: (selectedFilter: string, tabs: string[]) => any;
 }
 
-const filterListInfo: FilterAttributes[] = [
-  { category: "txnStatus", options: ["all", "paid", "upcoming", "failed"] },
-  { category: "status", options: ["active", "paused", "cancelled"] },
-];
-
-const computeBadgesFn = (selectedFilter: string, tabs: string[]) => {
-  return tabs.map((tab, index) => ({ option: tab, badge: index }));
+const renderTab = (tabData: any) => {
+  return (
+    <Badge
+      badgeContent={
+        <Typography variant="body1" color="#FFFFFF">
+          {tabData.badge}
+        </Typography>
+      }
+      color="primary"
+      slotProps={{
+        root: { style: { gap: 4, alignItems: "center" } },
+        badge: {
+          style: {
+            position: "static",
+            transform: "none",
+          },
+        },
+      }}
+    >
+      <Typography variant="body1">{tabData.text}</Typography>
+    </Badge>
+  );
 };
 
-const FilterTabs: React.FC<FilterTabsProps> = ({
-  filterList = filterListInfo,
-  handleFilterChange,
-  handleTabChange,
-  computeBadges = computeBadgesFn,
-}) => {
-  const [filter, setFilter] = useState<any>(filterList[0]);
+const tabListInfo = [
+  { label: "all", component: renderTab({ badge: 12, text: "All" }) },
+  { label: "paid", component: renderTab({ badge: 9, text: "Paid" }) },
+  {
+    label: "upcoming",
+    component: renderTab({ badge: 3, text: "Upcoming" }),
+  },
+  {
+    label: "failed",
+    component: renderTab({ badge: 0, text: "Failed" }),
+  },
+];
 
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+// const filterListInfo: FilterAttributes[] = [
+//   { category: "txnStatus", options: ["all", "paid", "upcoming", "failed"] },
+//   { category: "status", options: ["active", "paused", "cancelled"] },
+// ];
 
-  const onFilterChange = (filter: string) => {
-    setFilter({ category: filter, option: "" });
-    if (handleFilterChange) {
-      handleFilterChange(filter);
+// const computeBadgesFn = (selectedFilter: string, tabs: string[]) => {
+//   return tabs.map((tab, index) => ({ option: tab, badge: index }));
+// };
+
+const FilterTabs = ({
+  tabList = tabListInfo,
+  onTabChange,
+}: // onFilterChange,
+// computeBadges = computeBadgesFn,
+FilterTabsProps) => {
+  // const [filter, setFilter] = useState<any>(filterList[0]);
+  const [tab, setTab] = useState<TabAttributes>(tabList[0]);
+
+  const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down("sm"));
+
+  // const onFilterChange = (filter: string) => {
+  //   setFilter({ category: filter, option: "" });
+  //   if (handleFilterChange) {
+  //     handleFilterChange(filter);
+  //   }
+  // };
+
+  const handleTabChange = (newValue: any) => {
+    setTab(newValue);
+    if (onTabChange) {
+      onTabChange(newValue);
     }
   };
 
-  const onTabChange = (newValue: any) => {
-    setFilter({ ...filter, option: newValue });
-    if (handleTabChange) {
-      handleTabChange(filter, newValue);
-    }
-  };
+  // const tabs = useMemo(() => {
+  //   if (filter.category) {
+  //     const options = filterList.find(
+  //       (listItem) => listItem.category === filter.category
+  //     )?.options;
+  //     if (computeBadges && options) {
+  //       return computeBadges(filter.category, options);
+  //     }
+  //     return options;
+  //   }
+  //   return [];
+  // }, [filter.category]);
 
-  const tabs = useMemo(() => {
-    if (filter.category) {
-      const options = filterList.find(
-        (listItem) => listItem.category === filter.category
-      )?.options;
-      if (computeBadges && options) {
-        return computeBadges(filter.category, options);
-      }
-      return options;
-    }
-    return [];
-  }, [filter.category]);
-
-  const capitalize = (value: string) => {
-    return value.charAt(0).toUpperCase() + value.slice(1);
-  };
-
-  console.log(tabs);
+  // const capitalize = (value: string) => {
+  //   return value.charAt(0).toUpperCase() + value.slice(1);
+  // };
 
   return (
     <Stack
@@ -100,8 +136,8 @@ const FilterTabs: React.FC<FilterTabsProps> = ({
         )}
       </Button>
       <Tabs
-        value={filter.option || false}
-        onChange={(e, v) => onTabChange(v)}
+        value={tab}
+        onChange={(e, v) => handleTabChange(v)}
         variant="scrollable"
         sx={{
           gap: 3,
@@ -112,30 +148,9 @@ const FilterTabs: React.FC<FilterTabsProps> = ({
         {tabs.map((tab: any, index: number) => (
           <Tab
             key={index}
-            onClick={() => onTabChange(tab?.option || tab)}
-            value={tab?.option || tab}
-            label={
-              <Badge
-                badgeContent={
-                  <Typography variant="body1">{tab?.badge}</Typography>
-                }
-                invisible={Boolean(tab?.badge)}
-                color="primary"
-                slotProps={{
-                  root: { style: { gap: 4, alignItems: "center" } },
-                  badge: {
-                    style: {
-                      position: "static",
-                      transform: "none",
-                    },
-                  },
-                }}
-              >
-                <Typography variant="body1" sx={{ textTransform: "none" }}>
-                  {capitalize(tab?.option || tab)}
-                </Typography>
-              </Badge>
-            }
+            // onClick={() => onTabChange(tab?.option || tab)}
+            value={tab.label}
+            label={tab.component}
           />
         ))}
       </Tabs>
